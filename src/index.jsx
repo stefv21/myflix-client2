935bbc19fe36ceae2135ecb8a03ff14402fca043
stefv21 <161451930+stefv21@users.noreply.github.com>
2024-12-29 17:39:20 -0500

    updated scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
-import Container from "react-bootstrap/Container";
+import { Container } from 'react-bootstrap';
 
 
 // Import statement to indicate that you need to bundle './index.scss';
