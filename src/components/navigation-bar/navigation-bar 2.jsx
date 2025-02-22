79dfd151aecf092adca81a1bb382521b0fa69fc6
stefv21 <161451930+stefv21@users.noreply.github.com>
2025-02-22 17:42:41 -0500

    updates

---
+++ src/components/navigation-bar/navigation-bar 2.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Routes, Route, Navigate, BrowserRouter as Router } from "react-router-dom";
+
+import "./navigation-bar.scss"; // Optional for custom styles
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          myFlix
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {user && <Nav.Link as={Link} to="/">Home</Nav.Link>}
+            {user && <Nav.Link as={Link} to="/profile">Profile</Nav.Link>}
+          </Nav>
+          <Nav>
+            {!user ? (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+              </>
+            ) : (
+              <Button variant="outline-light" onClick={onLoggedOut}>
+                Logout
+              </Button>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
